0269424f0638aed9eb9f9c38f37c98c7d7813892
LakshmiKendre <lakshmi.patil.nine@gmail.com>
2020-02-18 12:08:20 +0530

    Modified: given method names proper

--- src/test/java/com/registration/UserRegistrationTest.java
+++ src/test/java/com/registration/UserRegistrationTest.java
@@ -5,27 +5,31 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     UserRegistration registration = new UserRegistration();
+
+    //test cases for FirstName
     @Test
-    public void testFirstName() {
+    public void givenFirstName_whenProper_shouldReturnTrue() {
         boolean name = registration.firstName("Lakshmi");
         Assert.assertEquals(true, name);
-
     }
 
+    //test cases for LastName
     @Test
-    public void testLastName() {
+    public void givenLastName_whenProper_shouldReturnTrue() {
         boolean name = registration.lastName("Patil");
         Assert.assertEquals(true, name);
     }
 
+    //test cases for Email address
     @Test
-    public void testEmail() {
+    public void givenEmail_whenProper_shouldReturnTrue() {
         boolean mail = registration.emailId("abc.xyz@bl.co.in");
         Assert.assertEquals(true, mail);
     }
 
+    // test cases for Mobile number
     @Test
-    public void testMobileNo() {
+    public void givenMobileNo_whenProper_shouldReturnTrue() {
         boolean mobile = registration.mobileNo("91 9876543210");
         Assert.assertEquals(true, mobile);
     }
